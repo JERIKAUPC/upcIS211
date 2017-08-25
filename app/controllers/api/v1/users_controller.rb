7120aff3191b2288a2b990edf0108f35d8a59d70
Oscar Pasache <oscar.pasache@gmail.com>
2017-08-24 20:54:49 -0500

    Agregar metodo eliminar usuario al servicio

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,18 +2,18 @@ module Api
     module V1
         class UsersController < ApplicationController
             skip_before_action :verify_authenticity_token
-            
+
             def index
                 users = User.order('created_at DESC')
                 render json: {status: 'SUCCESS', message: 'Loaded users', data:users},status: :ok
             end
-            
+
             def show
                 user = User.find(params[:id])
                 render json: {status: 'SUCCESS', message: 'Loaded user', data:user},status: :ok
             end
-            
-            def create 
+
+            def create
                 user = User.new(user_params)
                 if user.save
                     render json: {status: 'SUCCESS', message: 'Created user', data:user},status: :ok
@@ -21,10 +21,14 @@ module Api
                     render json: {status: 'ERROR', message: 'User not saved', data:user},status: :unprocessable_entity
                 end
             end
-            
-            def delete
+
+            def destroy
+                user = User.find(params[:id])
+
+                user.destroy
+                render json: {status: 'SUCCESS', message: 'Deleted user', data:user},status: :ok
             end
-            
+
             private
             def user_params
                 params.permit(:name,:email,:password,:phone)
